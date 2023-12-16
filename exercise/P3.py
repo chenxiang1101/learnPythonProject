3cccbb685f6b3c0eacd204d9d593caddf311cce4
chenxiang1101 <chen929xiang@gmail.com>
2023-12-16 21:54:28 +0800

    P3:列表到字典的函数

--- exercise/P3.py
+++ exercise/P3.py
@@ -1,6 +1,16 @@
+def addListToDict(inventory,addItems):
+    for key in addItems:
+        if key in inventory.keys():
+            inventory[key] += 1
+        else:
+            inventory[key] = 1
+    return inventory
+
 def displayInventory(dicList):
     for k,v in dicList.items():
         print(f"{v} {k}\n")
-dicList={'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-#调用方法
-displayInventory(dicList)
+
+inventory={'gold coin':42,'rope':1}
+addItems=['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+displayInventory(addListToDict(inventory,addItems))
